cc044b0577b97e14bee9d4d95b63001c08c41ea0
lishan <895977304@qq.com>
2019-02-12 21:11:40 +0800

    采用腾讯内核技术

--- app/src/main/java/com/example/administrator/lv/Act_Main.java
+++ app/src/main/java/com/example/administrator/lv/Act_Main.java
@@ -21,7 +21,9 @@ import android.os.Parcelable;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
@@ -34,10 +36,17 @@ import android.widget.Toast;
 import com.example.administrator.lv.permission.RxPermissions;
 import com.example.administrator.lv.utils.X5WebView;
 import com.lykj.aextreme.afinal.common.BaseActivity;
+import com.lykj.aextreme.afinal.utils.ACache;
+import com.lykj.aextreme.afinal.utils.Debug;
+import com.lykj.aextreme.afinal.utils.MyToast;
+import com.tencent.smtt.sdk.CookieSyncManager;
 
 import java.io.File;
+import java.net.HttpCookie;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import io.reactivex.functions.Consumer;
 
@@ -49,7 +58,9 @@ public class Act_Main extends BaseActivity {
     private RxPermissions rxPermissions;
     private ValueCallback<Uri> mUploadMessage;// 表单的数据信息
     private ValueCallback<Uri[]> mUploadCallbackAboveL;
-    private String myUrl="https://lawyer.libawall.com";
+    private String myUrl = "https://lawyer.libawall.com";
+    private ACache aCache;
+
     @Override
     public int initLayoutId() {
         return R.layout.act_main;
@@ -58,6 +69,11 @@ public class Act_Main extends BaseActivity {
     @Override
     public void initView() {
         hideHeader();
+        aCache = ACache.get(this);
+        if (aCache.getAsString("cookies") != null) {
+//            setCookies(aCache.getAsString("cookies"));
+            synCookies(this, myUrl);
+        }
         rxPermissions = new RxPermissions(this);
         rxPermissions
                 .request(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.CALL_PHONE)
@@ -91,6 +107,7 @@ public class Act_Main extends BaseActivity {
                 mUploadMessage = uploadMsg;
                 take();
             }
+
             // For Android  >= 5.0
             public boolean onShowFileChooser(com.tencent.smtt.sdk.WebView webView,
                                              com.tencent.smtt.sdk.ValueCallback<Uri[]> filePathCallback,
@@ -336,16 +353,7 @@ public class Act_Main extends BaseActivity {
     public static boolean isMediaDocument(Uri uri) {
         return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        CookieManager cookieManager = CookieManager.getInstance();
-        String cookieStr = cookieManager.getCookie(getDomain(myUrl));
-        SharedPreferences preferences = getSharedPreferences("cookie", Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("cookies", cookieStr);
-        editor.commit();
-    }
+
     /**
      * 获取URL的域名
      */
@@ -355,5 +363,48 @@ public class Act_Main extends BaseActivity {
             url = url.substring(0, url.indexOf('/'));
         }
         return url;
+    }
+
+    boolean myFinsh = false;
+
+    //使用Webview的时候，返回键没有重写的时候会直接关闭程序，这时候其实我们要其执行的知识回退到上一步的操作
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //这是一个监听用的按键的方法，keyCode 监听用户的动作，如果是按了返回键，同时Webview要返回的话，WebView执行回退操作，因为mWebView.canGoBack()返回的是一个Boolean类型，所以我们把它返回为true
+        if (keyCode == KeyEvent.KEYCODE_BACK && mWebView.canGoBack()) {
+            mWebView.goBack();
+            myFinsh = false;
+            return true;
+        } else {
+            if (myFinsh) {
+                finish();
+            } else {
+                MyToast.show(context, "您确定要退出App吗？");
+                myFinsh = true;
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        CookieManager cookieManager = CookieManager.getInstance();
+        String cookieStr = cookieManager.getCookie(getDomain(myUrl));
+        Log.e("------", "Cookies = " + cookieStr);
+        aCache.put("cookies", cookieStr);
+    }
+    /**
+     * 同步一下cookie
+     */
+    public void synCookies(Context context, String url) {
+        CookieSyncManager.createInstance(context);
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.removeSessionCookie();//移除
+        Debug.e("-----" + aCache.getAsString("cookies"));
+        cookieManager.setCookie(url, aCache.getAsString("cookies"));//cookies是在HttpClient中获得的cookie
+        CookieSyncManager.getInstance().sync();
     }
 }
--- app/src/main/java/com/example/administrator/lv/utils/X5WebView.java
+++ app/src/main/java/com/example/administrator/lv/utils/X5WebView.java
@@ -19,16 +19,10 @@ import com.tencent.smtt.sdk.WebViewClient;
 
 public class X5WebView extends WebView {
 	Context context;
-	TextView title;
-
 	public void setContext(Context context) {
 		this.context = context;
 	}
-
 	private WebViewClient client = new WebViewClient() {
-		/**
-		 * 防止加载网页时调起系统浏览器
-		 */
 		public boolean shouldOverrideUrlLoading(WebView view, String url) {
 			if (url.startsWith("http:") || url.startsWith("https:")) {
 				view.loadUrl(url);
@@ -45,15 +39,12 @@ public class X5WebView extends WebView {
 	public X5WebView(Context arg0, AttributeSet arg1) {
 		super(arg0, arg1);
 		this.setWebViewClient(client);
-		// this.setWebChromeClient(chromeClient);
-		// WebStorage webStorage = WebStorage.getInstance();
 		initWebViewSettings();
 		this.getView().setClickable(true);
 	}
 
 	private void initWebViewSettings() {
 		WebSettings webSetting = this.getSettings();
-		webSetting.setJavaScriptEnabled(true);
 		webSetting.setJavaScriptCanOpenWindowsAutomatically(true);
 		webSetting.setAllowFileAccess(true);
 		webSetting.setLayoutAlgorithm(LayoutAlgorithm.NARROW_COLUMNS);
@@ -61,19 +52,12 @@ public class X5WebView extends WebView {
 		webSetting.setBuiltInZoomControls(true);
 		webSetting.setUseWideViewPort(true);
 		webSetting.setSupportMultipleWindows(true);
-		// webSetting.setLoadWithOverviewMode(true);
 		webSetting.setAppCacheEnabled(true);
-		// webSetting.setDatabaseEnabled(true);
 		webSetting.setDomStorageEnabled(true);
 		webSetting.setGeolocationEnabled(true);
 		webSetting.setAppCacheMaxSize(Long.MAX_VALUE);
-		// webSetting.setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);
 		webSetting.setPluginState(WebSettings.PluginState.ON_DEMAND);
-		// webSetting.setRenderPriority(WebSettings.RenderPriority.HIGH);
 		webSetting.setCacheMode(WebSettings.LOAD_NO_CACHE);
-
-		// this.getSettingsExtension().setPageCacheCapacity(IX5WebSettings.DEFAULT_CACHE_CAPACITY);//extension
-		// settings 的设计
 	}
 
 	public X5WebView(Context arg0) {
